7951a9da033f28d2fb137e35e1e0f54c44fcaecd
yashchugh03 <142442603+yashchugh03@users.noreply.github.com>
2024-12-17 20:01:22 +0530

    Update allowedOrigins.js
    cors

--- MIndConnect/backend/config/allowedOrigins.js
+++ MIndConnect/backend/config/allowedOrigins.js
@@ -3,7 +3,8 @@ const allowedOrigins = [
     'http://127.0.0.1:5500',
     'http://localhost:3001',
     'http://localhost:3000',
-    'https://mind-connect-project.netlify.app'
+    'https://mind-connect-project.netlify.app',
+    'https://mind-connnect-project.netlify.app'
 ];
 
 module.exports = allowedOrigins;
